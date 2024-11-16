0b1a1e8a2c7ce8573ad8976caad1288176c84883
Prof-ademir <ademirdefreitas2020@gmail.com>
2024-11-16 09:50:19 -0300

    Add files via upload

---
+++ assets/img/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'programação',
+    'O que é javascript?',
+    'Javascript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de códigos de executas algumas tarefas'
+)
